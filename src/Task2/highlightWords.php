4b7453f27378b8c9e6103916cbc4c103759bbb86
Андрей Килязов <a.kilyazov@tapir.ws>
2024-06-11 12:20:56 +0300

    fix highlightWords function to highlight only the first occurrence of given word

--- src/Task2/highlightWords.php
+++ src/Task2/highlightWords.php
@@ -2,14 +2,29 @@
 
 namespace Task2;
 
+/**
+ * Есть строка $text в UTF-8 кодировке и массив слов $array_of_words (в той же самой кодировке). 
+ * Необходимо выделить первые вхождения каждого из слов с помощью квадратных скобок (Вася заменить на [Вася]), при этом не учитывать регистр (выделять как вася так и ваСя). 
+ * Выделить нужно только целые слова, а не подслова. Например, есть строка «Мама мыла раму» и массив «ама», «раму». В результате должно получиться «Мама мыла [раму]», а не «М[ама] мыла [раму]». 
+ */
 function highlightWords(string $text, array $array_of_words): string
 {
     $array_of_words = array_map('mb_strtolower', $array_of_words);
+    $array_of_words = array_combine($array_of_words, $array_of_words);
+
     $words = explode(' ', $text);
 
     foreach ($words as &$word) {
-        if (in_array(mb_strtolower($word), $array_of_words)) {
+        if ($array_of_words === []) {
+            break;
+        }
+
+        $lowercase_word = mb_strtolower($word);
+
+        if (in_array($lowercase_word, $array_of_words)) {
             $word = "[{$word}]";
+
+            unset($array_of_words[$lowercase_word]);
         }
     }
 
